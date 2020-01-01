ea63a9ff81cf12465bf6527cc363981903f7fe3e
Islam Sayed <islam.sayed8@gmail.com>
2020-01-01 08:35:08 +0200

    add 2 solutions

---
+++ missingNumbers/missingNumbers.js
@@ -0,0 +1,36 @@
+/*Write a function to return missing numbers from an unsorted array
+ *of numbers that may contain duplicates. 
+ *For example: findMissingNums([2, 6, 4, 8, 6]) should return [3, 5, 7]
+ */
+
+ // First solution
+function findMissingNums(arr) {
+    let missingNums = []
+    let subtractResult
+    arr.sort((a, b) => a - b)
+    for (let x = 0; x < arr.length; x++) {
+        subtractResult = arr[x + 1] - arr[x]
+        if (subtractResult > 1) {
+            for (let y = 1; y < subtractResult; y++) {
+                missingNums.push(arr[x] + y)
+            }
+        }
+    }
+    console.log(missingNums)
+    return missingNums
+}
+ 
+/*********************************************************/
+
+//Second Solution
+function findMissingNums(arr) {
+    let missingNums = []
+    arr.sort((a, b) => a - b)
+    for (let x = arr[0]; x <= arr[arr.length - 1]; x++) {
+        if (!arr.find(num => num === x)) {
+          missingNums.push(x);
+        }
+    }
+    console.log(missingNums)
+    return missingNums
+}
