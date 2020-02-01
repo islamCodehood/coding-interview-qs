cc519e7faf7afa2cb7e81db8ed1dce0725a9af4a
Islam Sayed <islam.sayed8@gmail.com>
2020-02-02 01:06:21 +0200

    add diagram

--- chunk/chunk.js
+++ chunk/chunk.js
@@ -1,11 +1,12 @@
 /*Write a function that takes 2 arguments. The first one is
- *an array and the second one is a number (chunk size).
- *The function should return an array of array chunks inside
- *with each array length is equal to the number (chunk size).*/
-
+*an array and the second one is a number (chunk size).
+*The function should return an array of array chunks inside
+*with each array length is equal to the number (chunk size).*/
 //Examples:
 //chunk([1,2,3,4,5], 2) //return [[1,2], [3,4], [5]]
-//chunk([3,7,8,1,2,4,10,13,12], 3) //return [[3,7,8], [1,2,4], [10,13,12]]
+//chunk([3,7,8,1,2,4,10,13,12], 3) //return [[3,7,8], [1,2,4], 
+//[10,13,12]]
+//chunk([1,2,3,4,5], 8) //return [[1,2,3,4,5]]
 
 //The first solution
 function chunk(arr, size) {
@@ -21,11 +22,13 @@ function chunk(arr, size) {
     return chunked;
 }
 
+
 //The second solution
 function chunk(arr, size) {
     const chunked = [];
     for (var i = 0; i < arr.length; i+= size) {
         chunked.push(arr.slice(i, i+size))
     }
+    console.log(chunked)
     return chunked;
 }
---
+++ firstDuplicate/firstDuplicateEdgeCase1.js
@@ -0,0 +1,28 @@
+/*Write a function that takes an argument of an array of
+ *numbers and returns the first duplicate.
+ *Example: findFirstDupl([2, 5, 4, 1, 3, 5, 4]) returns 4
+ */
+
+function findFirstDupl(arr) {
+    let numMap = {}
+    let minIndex = arr.length
+    for (let i = 0; i < arr.length; i ++) {
+        if (!numMap[arr[i]]) {
+            numMap[arr[i]]= {
+                count: 1,
+                index: i
+            }
+        } else {
+            numMap[arr[i]]['count'] ++
+        }
+    }
+    console.log(numMap)
+    for (const num in numMap) {
+        if (numMap[num]['count'] === 2) {
+            if (numMap[num]['index'] < minIndex) {
+                minIndex = numMap[num]['index']
+            }
+        }
+    }
+    return arr[minIndex]
+}
