fdd6da322313232964ea959c20f2d2c13edcc5c1
Islam Sayed <islam.sayed8@gmail.com>
2020-02-10 10:09:51 +0200

    remove seconf solution

--- anagram/anagram.js
+++ anagram/anagram.js
@@ -1,3 +1,12 @@
+/*Write a function that takes 2 string arguments and check if 
+ *the 2 strings are anagrams to each other (have the same characters
+ * with the same quantity). Consider only characters not spaces, or
+ * punctuations.*/
+
+//Example: anagram('Ab bc', 'cbab'); // return true
+//Example: anagram('abbc', 'cba'); // return false
+//Example: anagram('abbc?!', 'cbab'); // return true
+
 //The first solution
 function anagram(stringA, stringB) {
   return cleanString(stringA) === cleanString(stringB);
@@ -11,34 +20,4 @@ function cleanString(str) {
     .sort()
     .join("");
   return cleanedStr;
-}
-
-//The second solution
-function anagram(stringA, stringB) {
-    const charMapA = buildCharMap(stringA);
-    const charMapB = buildCharMap(stringB);
-
-    if(Object.keys(charMapA).length !== Object.keys(charMapB).length) {
-        return false;
-    }
-    for (const char in charMapA) {
-        if (charMapA[char] !== charMapB[char]) {
-            return false;
-        }
-    }
-    return true;
-}
-
-function buildCharMap(str) {
-    let charMap = {};
-    const cleanedStr = str.replace(/[^\w]|_/g, "").toLowerCase()
-    for (const char of cleanedStr) {
-        //charMap[char] = charMap[char] ? charMap[char] + 1 : 1;
-        if (charMap[char]) {
-          charMap[char] ++
-        } else {
-          charMap[char] = 1
-        }
-    }
-    return charMap;
 }
