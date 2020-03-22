e438653899169b66ff79d071430c9dda5ebede6d
Islam Sayed <islam.sayed8@gmail.com>
2020-03-22 18:10:30 +0200

    add question

---
+++ pyramid/pyramid-sol1.js
@@ -0,0 +1,12 @@
+/*Write a function that takes a number argument, and
+ *prints a pyramid shape with n levels using the #
+ *character like in these examples:
+ *pyramid(1) '#'
+ *
+ *pyramid(2) ' # '
+ *           '###'
+ *
+ *pyramid(3) '  #  '
+ *           ' ### '
+ *           '#####'
+ */
